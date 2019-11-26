6833d1504bb5efcc3f5967601a93db935118fa78
marcosgmgm <marcosgmgm@gmail.com>
2019-11-26 11:31:12 -0300

    Creating microservice scaffold

---
+++ src/main/kotlin/br/com/zup/testecli/DemoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.testecli
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
